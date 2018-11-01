1eb2d4fb233f2a1e933164922344a8b25f844c4f
Hanicef <gustaf.alhall@yahoo.com>
2018-11-01 13:13:19 +0100

    Add entity and repository

---
+++ src/main/java/com/tobenamed/platform/PlatformRepository.java
@@ -0,0 +1,8 @@
+
+package com.tobenamed.platform;
+
+import org.springframework.data.domain.*;
+import org.springframework.data.repository.*;
+
+public interface PlatformRepository extends CrudRepository<Platform, Integer> {
+}
