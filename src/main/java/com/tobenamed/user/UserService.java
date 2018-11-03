7c83d36e3851c830928f5a6c7172371cedc3f614
Arasto Sahbaei <Arasto.Sahbaei@gmail.com>
2018-11-03 21:08:42 +0100

    #10 Service for users created

--- src/main/java/com/tobenamed/user/UserService.java
+++ src/main/java/com/tobenamed/user/UserService.java
@@ -1,4 +1,45 @@
 package com.tobenamed.user;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService implements UserServiceInterface {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private List<UserModel> convertUserListToUserModelList(List<UserEntity> userList) {
+        List<UserModel> userModelList = new ArrayList<>();
+        for (UserEntity userEntity : userList) {
+            userModelList.add(new UserModel(userEntity));
+        }
+        return userModelList;
+    }
+
+    public UserModel createNewUser(UserModel userModel) {
+        UserEntity user = new UserEntity(userModel);
+        return new UserModel(userRepository.save(user));
+    }
+
+    public List<UserModel> findAllUsers() {
+        List<UserEntity> ListWithAllUsers = userRepository.findAll();
+        return convertUserListToUserModelList(ListWithAllUsers);
+    }
+
+    public UserModel FindUserById(Long id) {
+        UserEntity userEntity = userRepository.getOne(id);
+        return new UserModel(userEntity);
+    }
+
+    public UserEntity FindUserByUserName(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+
 }
--- src/main/java/com/tobenamed/user/UserServiceInterface.java
+++ src/main/java/com/tobenamed/user/UserServiceInterface.java
@@ -1,4 +1,15 @@
 package com.tobenamed.user;
 
+import java.util.List;
+
 public interface UserServiceInterface {
+
+    List<UserModel> findAllUsers();
+
+    UserModel FindUserById(Long id);
+
+    UserEntity FindUserByUserName(String username);
+
+    UserModel createNewUser(UserModel userModel);
+
 }
