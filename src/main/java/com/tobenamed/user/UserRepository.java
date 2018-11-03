cd4b8aee8471874b2af1843c58dc4278310049ff
Arasto Sahbaei <Arasto.Sahbaei@gmail.com>
2018-11-03 20:59:15 +0100

    #10 added repository & updated gitIgnore

--- src/main/java/com/tobenamed/user/UserRepository.java
+++ src/main/java/com/tobenamed/user/UserRepository.java
@@ -1,4 +1,10 @@
 package com.tobenamed.user;
 
-public interface UserRepository {
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+    @Repository
+    public interface UserRepository extends JpaRepository<UserEntity, Long> {
+        UserEntity findByUsername(String username);
+    }
+
