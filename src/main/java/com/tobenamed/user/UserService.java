61f0f893afd6758aaaada3af264260d5345e2100
Arasto Sahbaei <Arasto.Sahbaei@gmail.com>
2018-11-04 20:39:32 +0100

    #10 fixed autowire issue with the rest api

--- src/main/java/com/tobenamed/user/UserService.java
+++ src/main/java/com/tobenamed/user/UserService.java
@@ -5,10 +5,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserService implements UserServiceInterface, UserDetailsService {
 
     @Autowired
