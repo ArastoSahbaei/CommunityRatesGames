8b1238394e6082b5c00470a6a4b49d7a24366509
Gustaf Alhäll <gustaf.alhall@yahoo.com>
2018-11-04 19:46:33 +0100

    Add platform service

--- src/main/java/com/tobenamed/platform/PlatformRepository.java
+++ src/main/java/com/tobenamed/platform/PlatformRepository.java
@@ -3,6 +3,9 @@ package com.tobenamed.platform;
 
 import org.springframework.data.domain.*;
 import org.springframework.data.repository.*;
+import org.springframework.stereotype.*;
 
+// Conflicts with org.springframework.data.repository.Repository, so be explicit.
+@org.springframework.stereotype.Repository
 public interface PlatformRepository extends CrudRepository<Platform, Integer> {
 }
