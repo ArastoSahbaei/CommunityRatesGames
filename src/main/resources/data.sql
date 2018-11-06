73da60f8150724d3e1cf58bd292ef7934bdc8ae5
Arasto Sahbaei <Arasto.Sahbaei@gmail.com>
2018-11-07 00:17:24 +0100

    24 created script for auto-population on startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_entity(id, email, first_name, last_name, password, role, user_name)
+VALUES
+(1, '1user@user.user', 'Arasto', 'Sahbaei', 'power', 'admin', 'arrebarre1337'),
+(2, '2user@user.user', 'Robin', 'Svensson', 'power', 'admin', 'RobinLiquid'),
+(3, '3user@user.user', 'Daniel', 'Ivestig', 'power', 'admin', 'Daniel'),
+(4, '4user@user.user', 'Gustaf', 'Cyborg', 'power', 'admin', 'HighQualityGurka');
