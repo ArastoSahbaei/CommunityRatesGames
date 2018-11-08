f3671d3126e6d988fee690a1d5bd12a4ded2e8d7
Robin <swesson92@gmail.com>
2018-11-08 15:29:22 +0100

    lagt till alla components och importerat HttpModuleClient till appmodule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MaterialModule } from "./shared/material/material.module";
+import {HttpClientModule} from "@angular/common/http";
+import { CompanyComponent } from './company/company.component';
+import { PlatformComponent } from './platform/platform.component';
+import { RatingComponent } from './rating/rating.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    NavigationComponent
+    NavigationComponent,
+    CompanyComponent,
+    PlatformComponent,
+    RatingComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
