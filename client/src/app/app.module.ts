c7f69a49d84a7367d63e1fc4f3bc0173737c0aaa
Arasto Sahbaei <Arasto.Sahbaei@gmail.com>
2018-11-09 21:00:18 +0100

    fixing routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,31 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { MaterialModule } from "./shared/material/material.module";
+import {AppComponent} from './app.component';
+import {GameComponent} from './game/game.component';
+import {MaterialModule} from "./shared/material/material.module";
 import {HttpClientModule} from "@angular/common/http";
-import { CompanyComponent } from './company/company.component';
-import { PlatformComponent } from './platform/platform.component';
-import { RatingComponent } from './rating/rating.component';
-import { UserComponent } from './user/user.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import {CompanyComponent} from './company/company.component';
+import {PlatformComponent} from './platform/platform.component';
+import {RatingComponent} from './rating/rating.component';
+import {UserComponent} from './user/user.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
+import {MainNavComponent} from './main-nav/main-nav.component';
 import {AppRoutingModule, routingComponents} from "./app-routing.module";
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ReportBugComponent } from './report-bug/report-bug.component';
+import { AddGameComponent } from './add-game/add-game.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    NavigationComponent,
     CompanyComponent,
     PlatformComponent,
     RatingComponent,
     UserComponent,
     MainNavComponent,
-    routingComponents
+    routingComponents,
+    RegisterComponent,
+    HomeComponent,
+    ReportBugComponent,
+    AddGameComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,4 +50,5 @@ import {AppRoutingModule, routingComponents} from "./app-routing.module";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ client/src/app/report-bug/report-bug.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReportBugComponent } from './report-bug.component';
+
+describe('ReportBugComponent', () => {
+  let component: ReportBugComponent;
+  let fixture: ComponentFixture<ReportBugComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ReportBugComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReportBugComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
