95aaa3d548667261e3c202902bc20e0fc72a34a4
Daniel Ivestig <exodius666@hotmail.com>
2018-11-15 11:48:40 +0100

    more test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,29 +1,39 @@
 INSERT INTO user_entity(id, email, password, role, user_name)
 VALUES
-(1, '1user@user.user', 'power', 'admin', 'arrebarre1337'),
+(1, '1user@user.user', 'power', 'admin', 'Harastio'),
 (2, '2user@user.user', 'power', 'admin', 'RobinLiquid'),
 (3, '3user@user.user', 'power', 'admin', 'Daniel'),
-(4, '4user@user.user', 'power', 'admin', 'HighQualityGurka');
+(4, '4user@user.user', 'power', 'admin', 'BostonGurka'),
+(5, 'that@dude.user', 'whut', 'user', 'Whatdude');
 
 INSERT INTO company_entity(id, company_name, country, city)
 VALUES
 (1, 'Microsoft', 'US', '???'),
-(2, 'Nintendo', 'Japan', 'GloriousNippon');
+(2, 'Nintendo', 'Japan', 'GloriousNippon'),
+(3, 'BasementDudes', 'Antarktis', 'North Pole');
 
 INSERT INTO game_entity(id, title, company_id)
 VALUES
 (1, 'Halo', 1),
-(2, 'Hallå', 1),
-(3, 'Hallon', 1),
-(4, 'Haloeller', 1),
-(5, 'Halloes', 1),
+(2, 'Hallå', 2),
+(3, 'Hallon', 2),
+(4, 'Haloeller', 2),
+(5, 'Halloes', 2),
 (6, 'Hajro II', 1),
-(7, 'Mario cart', 2);
+(7, 'Mario cart', 4);
+(8, 'Mario party', 4)
+(9, 'Final Fantasy 1337', 3)
 
 INSERT INTO platform_entity(id, name, release_year, company_id)
 VALUES
-(1, 'XBox', 2001, 1);
+(1, 'XBox', 2001, 1),
+(2, 'PC', NULL, NULL),
+(3, 'Playstation', 1998, 2),
+(4, 'Nintendo', 1984, 3);
 
 INSERT INTO rating_entity(id, user_id, game_id, rating)
 VALUES
-(1, 1, 1, 5);
+(1, 1, 1, 5),
+(2, 2, 1, 3),
+(3, 3, 1, 0),
+(4, 4, 1, 3);
