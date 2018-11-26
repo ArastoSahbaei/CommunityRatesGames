b7a78514420f41e8e02e6023b5bfe1d50df40490
Arasto <Arasto.Sahbaei@gmail.com>
2018-11-26 14:47:00 +0100

    another api update

--- client/src/app/shared/service/api.service.ts
+++ client/src/app/shared/service/api.service.ts
@@ -1,9 +1,9 @@
 import {AddGame} from "../interface/add-game.interface";
-import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Headers } from "./headers";
-import { UrlService } from "./url.service";
-import { Register } from "../interface/register.interface";
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Headers} from "./headers";
+import {UrlService} from "./url.service";
+import {Register} from "../interface/register.interface";
 import {User} from "../interface/user.interface";
 
 
@@ -13,31 +13,32 @@ import {User} from "../interface/user.interface";
 export class ApiService {
 
   constructor(private httpClient: HttpClient,
-              private url:UrlService) { }
+              private url: UrlService) {
+  }
 
   searchGameByTitle() {
-    return this.httpClient.get(this.url.getBaseUrl()+this.url.getSearchForGame(), {headers: Headers.HeaderJSON()});
+    return this.httpClient.get(this.url.getBaseUrl() + this.url.getSearchForGame(), {headers: Headers.HeaderJSON()});
   }
 
   getGames() {
-    return this.httpClient.get(this.url.getBaseUrl()+this.url.getGames(), {headers: Headers.HeaderJSON()});
+    return this.httpClient.get(this.url.getBaseUrl() + this.url.getGames(), {headers: Headers.HeaderJSON()});
   }
 
   getRating() {
-    return this.httpClient.get(this.url.getBaseUrl() + this.url.getRating(), {headers: Headers.HeaderJSON() } )
+    return this.httpClient.get(this.url.getBaseUrl() + this.url.getRating(), {headers: Headers.HeaderJSON()});
   }
 
   checkCredentials(body: User) {
-    return this.httpClient.post(this.url.getBaseUrl() + this.url.getCredential(), body, {headers: Headers.HeaderJSON() } );
+    return this.httpClient.post(this.url.getBaseUrl() + this.url.getCredential(), body, {headers: Headers.HeaderJSON()});
   }
 
   registerUser(body: Register) {
-    return this.httpClient.post(this.url.getBaseUrl() + this.url.getUser(), body, {headers: Headers.HeaderJSON() } );
+    return this.httpClient.post(this.url.getBaseUrl() + this.url.getUser(), body, {headers: Headers.HeaderJSON()});
   }
 
-  postGame(body: AddGame){
+  postGame(body: AddGame) {
     console.log(body);
-  return this.httpClient.post(this.url.getBaseUrl()+ this.url.getGames(), body, {headers: Headers.HeaderJSON()}
+    return this.httpClient.post(this.url.getBaseUrl() + this.url.getGames(), body, {headers: Headers.HeaderJSON()}
     );
   }
 
