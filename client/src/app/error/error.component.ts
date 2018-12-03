e3a8a94bddaaf6a6228450ec3d3bbf629a1dfaf3
Björn <bjorn.alfsson@gmail.com>
2018-12-03 17:27:53 +0100

    Added all error codes

--- client/src/app/error/error.component.ts
+++ client/src/app/error/error.component.ts
@@ -18,11 +18,45 @@ export class ErrorComponent implements OnInit {
 
   showHelpfulErrorMessage(errorMessage: string) {
       switch (errorMessage) {
+        case '400':
+          // Bad Request
+          this.errorMessage = "Sorry, we couldn't understand what you were sending to us!";
+          break;
+        case '401':
+          // Unauthorized
+          this.errorMessage = "You do not have access rights here";
+          break;
+        case '403':
+          // Forbidden
+          this.errorMessage = "This section is forbidden for you.";
+          break;
         case '404':
+          // Not found
           this.errorMessage = "We couldn't find the data you were looking for!";
           break;
         case '406':
+          // Not Acceptable
           this.errorMessage = "We couldn't process your data!";
+          break;
+        case '408':
+          // Request Timeout
+          this.errorMessage = "Your request timed out. Please try again";
+          break;
+        case '409':
+          // Conflict
+          this.errorMessage = "Your request conflicted somewhere. Please try again";
+          break;
+        case '410':
+          // Gone
+          this.errorMessage = "Whatever you were looking for is now gone in the wind!";
+          break;
+        case '500':
+          // Internal Server Error
+          this.errorMessage = "The server did not respond. Please wait while we try to set it back up!";
+          break;
+        case '503':
+          //  Service Unavailable
+          this.errorMessage = "Right now we have too many requests pending. Please try again later";
           break;
       }
   }
