a3806d664faf5bdcebc8f30e2c9ba1e958c09b3e
Robin <swesson92@gmail.com>
2018-12-12 15:36:32 +0100

    you can now update rating on games that exists

--- client/src/app/shared/interface/add-game.interface.ts
+++ client/src/app/shared/interface/add-game.interface.ts
@@ -2,7 +2,9 @@ export interface AddGame{
     title: String;
     companyId: number;
     allPlatformId: number[];
-
+    game: String;
+    user: String;
+    rating: number;
 
   //  company: any;
 //  platforms: any;
