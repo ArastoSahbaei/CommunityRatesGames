905baf91e7f3b242325d8a6bd9e3c6fd6233bcf0
Björn <bjorn.alfsson@gmail.com>
2018-12-13 14:45:46 +0100

    New npm install

--- src/main/java/com/communityratesgames/transactions/GameService.java
+++ src/main/java/com/communityratesgames/transactions/GameService.java
@@ -68,17 +68,12 @@ public class GameService implements GameDataAccess {
 
     @Override
     public String searchFiveGames(String query) {
-        StoredProcedureQuery searchForFiveGamesByTitle =
-                em.createNamedStoredProcedureQuery("searchForFiveGamesByTitle");
 
-        StoredProcedureQuery sp =
-                searchForFiveGamesByTitle.setParameter("query",query);
-        /*
             List<Game> results = em.createQuery("SELECT g FROM Game g WHERE g.title LIKE :title AND g.verified = TRUE",Game.class)
                     .setParameter("title", query+'%')
                     .setMaxResults(5)
-                    .getResultList();*/
-            return reduceGameToTitleAndId(sp.getResultList());
+                    .getResultList();
+            return reduceGameToTitleAndId(results);
     }
 
     @Override
