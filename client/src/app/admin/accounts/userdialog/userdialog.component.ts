dc7f73f571a491e187111873c562d37a143bbee6
Björn <bjorn.alfsson@gmail.com>
2018-12-16 12:52:47 +0100

    frontend reactive form is ready and working

--- client/src/app/admin/accounts/userdialog/userdialog.component.ts
+++ client/src/app/admin/accounts/userdialog/userdialog.component.ts
@@ -1,9 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ApiService} from "../../../shared/service/api.service";
-import {User} from "../../../shared/interface/user.interface";
-import {finalize} from "rxjs/operators";
+
 
 @Component({
   selector: 'app-userdialog',
@@ -13,33 +12,29 @@ import {finalize} from "rxjs/operators";
 export class UserdialogComponent implements OnInit {
 
   public userDetails: FormGroup;
-  public isReady: boolean;
-  private user: User;
 
   constructor(public dialogRef: MatDialogRef<UserdialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: object,
               private fb: FormBuilder,
               private api: ApiService) {
 
-    this.api.getUserDetails(this.data["user"]).subscribe((response) => {
-      console.log(response);
-      this.user.username = response["userName"];
-      this.user.email = response["email"];
-      console.log(this.user);
-    },
-      finalize(() => this.isReady = true)
-    );
+    this.userDetails = new FormGroup({
+      'name': new FormControl('', [Validators.required]),
+      'mail': new FormControl('', [Validators.required, Validators.email]),
+      'action': new FormControl('', Validators.required)
+    });
+
   }
 
   ngOnInit() {
-
-    this.userDetails = this.fb.group({
-      'name': [console.log(this.user.username), Validators.required],
-      'mail': ['Hej Hopp', [Validators.required, Validators.email]],
-      'action': ['', Validators.required]
-    });
+    this.api.getUserDetails(this.data["user"]).subscribe(
+      response =>
+        this.userDetails.patchValue({
+          name: response["userName"],
+          mail: response["email"]
+        }),
+      )
   }
-
   onSubmit(){
 //    this.api.getUserDetails()
     console.log(this.userDetails.value);
