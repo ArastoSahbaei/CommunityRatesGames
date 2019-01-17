7b693de8b4b901e10eb720c7eb48f713018c01b8
Björn <bjorn.alfsson@gmail.com>
2019-01-17 11:25:12 +0100

    Fixed imports to avoid rxjs-compat

--- client/src/app/auth-interceptor.ts
+++ client/src/app/auth-interceptor.ts
@@ -1,16 +1,16 @@
 import {Injectable,Injector} from '@angular/core';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs/Rx';
 import {AuthService} from './shared/service/auth.service'
 import {StorageService} from "./shared/service/storage.service";
 import {Storage} from "./shared/interface/storage.interface";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor{
   constructor(private storage: StorageService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    var token: string = this.storage.getItem('token');
+    let token: string = this.storage.getItem('token');
     if (token == null) {
       return next.handle(req);
     } else {
